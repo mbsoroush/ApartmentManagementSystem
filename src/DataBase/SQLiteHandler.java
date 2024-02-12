b557044d13e71028c01e2c0807c380db33f662b6
Mohammad Bagher Soroush <108808605+mbsoroush@users.noreply.github.com>
2024-02-12 12:27:01 +0330

    Update SQLiteHandler.java

--- src/DataBase/SQLiteHandler.java
+++ src/DataBase/SQLiteHandler.java
@@ -1,5 +1,5 @@
 package DataBase;
-/*import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
@@ -141,7 +141,7 @@ public class SQLiteHandler {
 		return status;
 	}
 }	
-*/
+
 
 
 
